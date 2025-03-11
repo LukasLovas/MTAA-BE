255d73a6f032e8b4f19d7bff7cba603eea5f395e
oleksandrsakalosh <orel.oleksander@gmail.com>
2025-03-11 16:26:40 +0100

    initial

---
+++ MTAA/src/main/java/com/example/mtaa/MtaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtaaApplication.class, args);
+    }
+
+}
